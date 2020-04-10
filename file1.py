0b338de8d059bc745c681ae4ee583bdc8d76f9c9
shahil04 <ansarirahish455@gmail.com>
2020-04-10 15:08:18 +0530

    day9

---
+++ file1.py
@@ -0,0 +1,6 @@
+#save this file as file1.py
+def fn():
+    return "do some stuff"
+
+def other_fn():
+    return "do some other stuff"
---
+++ file2.py
@@ -0,0 +1,5 @@
+import file1
+
+file1.fn() # 'do some stuff'
+
+#file2.fn() # 'do some other stuff'
